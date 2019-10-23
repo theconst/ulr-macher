f47176c6c0988afdbb6da9317ba020fe2d35ae69
kko <kkovalchuk2017@gmail.com>
2019-10-23 10:49:56 +0200

    Move proto repl dependency to development

--- project.clj
+++ project.clj
@@ -4,8 +4,6 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [proto-repl "0.3.1"]
-                 [proto-repl-charts "0.3.1"]
 
                  [clj-antlr "0.2.5"]
 
@@ -13,4 +11,6 @@
                  [ch.qos.logback/logback-classic "1.2.3"]]
   :main ^:skip-aot url-matcher.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}
+             :dev {:dependencies [[proto-repl "0.3.1"]
+                                  [proto-repl-charts "0.3.1"]]}})
